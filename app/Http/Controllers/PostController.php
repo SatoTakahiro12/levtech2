a0c3680dda51e4bca42d9006f72c1e32941bfb8e
SatoTakahiro12 <inazumabulek@ezweb.ne.jp>
2023-04-17 15:28:53 +0900

    ブログ投稿作成画面作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -11,5 +12,22 @@ class PostController extends Controller
     {
         return view('posts/index')->with(['posts' => $post->getPaginateBYLimit()]);  
     }
-}
+      
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+    return view('posts/create');
+     }
+     
+     public function store(PostRequest $request, Post $post)
+     {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+     }
+}     
 ?>
